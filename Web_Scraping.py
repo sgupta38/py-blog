cdfaf08b13586a44506694dba053803c5aabd849
sonugupta4636 <sonugupta4636@gmail.com>
2018-01-10 22:34:45 +0530

    Modified example for calculating share price.
     * Added dictionary data structure

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -5,11 +5,25 @@
 
 import bs4, requests
 
-res = requests.get('http://www.moneycontrol.com/india/stockpricequote/computers-software/quickhealtechnologies/QHT')
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-elems = soup.select('#Bse_Prc_tick > strong')
-# price = elems[0].text.strip()
-#
-# print('Price of the item is: ' + price)
-print(elems[0].text.strip())
+def findSharePrice(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    bseElems = soup.select('#Bse_Prc_tick > strong')
+    nseElems = soup.select('#Nse_Prc_tick > strong')
+    Price = {
+                'BSE': bseElems[0].text.strip(),
+                'NSE': nseElems[0].text.strip(),
+        }
+    return Price
+
+    # #
+    # # print('Price of the item is: ' + price)
+    # print(' BSE price: ' + bseElems[0].text.strip())
+    # #
+    # # print('Price of the item is: ' + price)
+    # print(' NSE price: ' + nseElems[0].text.strip())
+
+SharePrice = findSharePrice('http://www.moneycontrol.com/india/stockpricequote/computers-software/quickhealtechnologies/QHT')
+print(' BSE price: ' + SharePrice['BSE'])
+print(' NSE price: ' + SharePrice['NSE'])
