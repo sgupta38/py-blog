9b1e7ff18fe1220a8c0b00ebca57c89f4a82e42a
sonugupta4636 <sonugupta4636@gmail.com>
2018-01-13 00:33:33 +0530

    Added example for creating and modifying spreadsheets.

--- pyexcel.py
+++ pyexcel.py
@@ -2,7 +2,6 @@
 # Columns[letters] & Row[numbers]
 ## todo: Writing to Spreadsheet
 
-
 import openpyxl
 import os
 
@@ -29,3 +28,40 @@ print(sheet['h4'].value)
 
 for i in range(52, 63):
     print(i, sheet.cell(row=i, column=3).value, sheet.cell(row=i, column=4).value)
+
+print()
+print()
+##########################################################################################################
+##                          Creating and modifying Spreadsheet
+##########################################################################################################
+
+wb = openpyxl.Workbook() # This creates new workbook
+print(wb.get_sheet_names())
+
+sheet = wb.get_sheet_by_name('Sheet')
+sheet['a1'] = 46;
+sheet['a2'] = 'Sonu Gupta';
+
+## Till now whatever we have done is saved in 'Memory'.
+# To save it on hard drive use 'save()'
+
+wb.save('mysheet1.xlsx')
+
+# Lets open the workbook for modifying
+wb = openpyxl.load_workbook('mysheet1.xlsx')
+
+#To add new Sheet
+sheet2 = wb.create_sheet()          # by default added to last
+print(wb.get_sheet_names())
+
+## If we want to change the name of sheet
+
+print(sheet2.title)
+sheet2.title = 'My New sheet name'
+print(wb.get_sheet_names())
+
+wb.save('mysheet1.xlsx')
+
+## To add at particular location
+sheet2 = wb.create_sheet(index=0, title='My Other sheet')          # by default added to last
+wb.save('mysheet1.xlsx')
