8ef59509d76bd9ad945e881168e2cf98088b898b
sonugupta4636 <sonugupta4636@gmail.com>
2018-02-02 01:25:28 +0530

    Added another technique for binding function to button.

--- GUI/func_binding.py
+++ GUI/func_binding.py
@@ -1,14 +1,26 @@
 from tkinter import *
 root = Tk()
 
-def printName():
-    print("Winter is coming..!!!!")
 
-##
+## Technique 1
 ## Here, we are binding the function "printName" with the button. So whenevr, button is clicked, "printName" function will b executed.
 ##
 ##  Notice the parameter 'command' is used for function binding
-button = Button(root, text="Click Me", command=printName)
+#def printName():
+#    print("Winter is coming..!!!!")
+#button = Button(root, text="Click Me", command=printName)
+#button.pack()
+
+
+## Technique 2
+## Here, we are binding the function "printName" with the button, based on some event such as 'mouseclick'
+##
+##  Notice the parameter 'command' is used for function binding
+def printName(event):
+    print("Winter is coming..!!!!")
+
+button = Button(root, text="Click Me")
+button.bind("<Button-1>", printName)   # Takes two parameter, 1st is mousebutton number, 2nd is 'function name' to call
 button.pack()
 
 root.mainloop()
